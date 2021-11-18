9d2cc57171453b4f7b91d2a76bda98c1c90c577d
EC2 Default User <ec2-user@ip-172-31-32-158.ap-southeast-2.compute.internal>
2021-11-18 22:48:29 +1100

    initial commit api - implemented web - implemented

---
+++ api/authorise.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+from typing import Optional
+from fastapi import FastAPI, Request
+import logging
+
+PREFIX = '/app/fac/api/'
+app = FastAPI(docs_url="/cgi/docs", redoc_url="/cgi/redoc", openapi_url="/cgi/openapi.json")
+db = {}
+
+@app.get("/")
+@app.get("/cgi")
+def read_root():
+    return {"msg": "hello world from fastapi"}
+
+
+@app.get("/app/fac/api/db")
+def read_db():
+    return db
+
+
+@app.get("/app/fac/api/login")
+def login(channel: str, request: Request):
+    ip = request.client.host
+    if db.get(ip) is not None and channel in db[ip]:
+        return True
+    return False
+
+
+@app.get("/app/fac/api/authorise")
+def authorise(channel: str, request: Request):
+    ip = request.client.host
+    if db.get(ip) is None:
+        db[ip] = []
+    length_pre = len(db[ip])
+    db[ip].append(channel)
+    db[ip] = list(dict.fromkeys(db[ip]))
+    if length_pre < len(db[ip]):
+        return True
+    return False
+
+
+@app.get("/app/fac/api/clear")
+def clear(request: Request):
+    ip = request.client.host
+    if db.get(ip) is not None:
+        db[ip] = []
+        return True
+    return False
---
+++ web/js/script.js
@@ -0,0 +1,104 @@
+function bodyonload() {
+    login();
+}
+
+async function generate_qrcode(url, channel) {
+    console.log('generate_qrcode: [' + channel + ']')
+    if (channel != null){
+        if (document.getElementById('channel').innerHTML == '')
+        {
+            create_qrcode(url, channel);
+            await sleep(1000);
+            login();
+        }
+        else
+        {
+            await sleep(1000);
+            login();
+        }
+    }
+    else
+        document.getElementById('error').innerHTML = 'channel parameter not found'
+}
+
+function create_qrcode(url, channel) {
+    document.getElementById('channel').innerHTML = 'Login to ' + channel;
+    console.log("create_qrcode: " + url);
+    new QRCode(document.getElementById("qrcode"), url);
+}
+
+
+function create_video() {
+    url = 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8';
+    video = document.createElement('video');
+    video.src = url;
+    video.width = 1280;
+    video.muted = true;
+    video.autoplay = true;
+    // video.controls = true;
+    // document.getElementById('video').appendChild(video);
+    document.body.appendChild(video);
+}
+
+function login() {
+    url = location.origin;
+    url += location.pathname.replace('index.html', 'api/login');
+    url += '?channel=' + getQueryParam('channel');
+
+    console.log('login: ' + url);
+
+    httpGet(url, function (response) {
+        console.log('login.response: ' + response);
+        if (response == "true")
+            create_video();
+        else
+            generate_qrcode(url.replace('login', 'authorise'), getQueryParam('channel'));
+    })
+}
+
+
+function getQueryParam(name) {
+    if (
+        (name = new RegExp(
+            "[?&]" + encodeURIComponent(name) + "=([^&]*)"
+            ).exec(location.search))
+        )
+        return decodeURIComponent(name[1]);
+}
+
+function httpGet(data, cb) {
+    var url = data;
+    var xmlHttp = new XMLHttpRequest();
+    var format = "json";
+
+    if (typeof data == "object") {
+        url = data.url;
+        if (data.format) {
+            format = "xml";
+        }
+    }
+
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            cb(format === "xml" ? xmlHttp.responseXML : xmlHttp.response);
+        }
+    };
+
+    xmlHttp.open("GET", url, true); // true for asynchronous
+
+    if (typeof data == "object" && data.apiHeaders) {
+        var key = null,
+            value = null;
+        for (key in data.apiHeaders) {
+            value = data.apiHeaders[key];
+            xmlHttp.setRequestHeader(key, value);
+        }
+    }
+
+    xmlHttp.send();
+}
+
+// sleep time expects milliseconds
+async function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
